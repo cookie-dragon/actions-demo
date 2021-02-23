e9959b5a3d5838ddb21231bd6f0a715111dd7306
Cooky.Long <lj12875@mail.haitian.com>
2021-02-23 22:12:57 +0800

    update

---
+++ htbox-forlinx/buildroot-add/package/python-pyotp/python-pyotp.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# python-pyotp
+#
+################################################################################
+
+PYTHON_PYOTP_VERSION = 2.6.0
+PYTHON_PYOTP_SOURCE = pyotp-$(PYTHON_PYOTP_VERSION).tar.gz
+PYTHON_PYOTP_SITE = https://files.pythonhosted.org/packages/61/cc/3f440f8ec7611e1252826d304f4807b25d1814c606037af31b8af50dcd80
+PYTHON_PYOTP_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
