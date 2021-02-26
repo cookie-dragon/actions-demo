6403738ed6039fd14ed2e3b87fe1a554f1f105bb
Cooky.Long <lj12875@mail.haitian.com>
2021-02-26 09:47:42 +0800

    u

--- htbox-forlinx/buildroot-add/package/python-htbox/python-htbox.mk
+++ htbox-forlinx/buildroot-add/package/python-htbox/python-htbox.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 PYTHON_HTBOX_VERSION = 1.0
-PYTHON_HTBOX_SITE = $(TOPDIR)/package/python-htbox/htbox-src
+PYTHON_HTBOX_SITE = $(TOPDIR)/package/python-htbox/htbox-1.0
 PYTHON_HTBOX_SITE_METHOD=local
 PYTHON_HTBOX_SETUP_TYPE = setuptools
 
