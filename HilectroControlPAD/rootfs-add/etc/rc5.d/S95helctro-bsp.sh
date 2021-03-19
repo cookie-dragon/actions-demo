a6d0c85fdceb2438df5d4ca2d137d41bd444dd56
Cooky.Long <lj12875@mail.haitian.com>
2021-03-19 14:07:10 +0800

    u

--- HilectroControlPAD/rootfs-add/etc/rc5.d/S95helctro-bsp.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-echo -n "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-#ifconfig eth0 172.18.11.71 netmask 255.255.255.0
-echo -n "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-mknod /dev/input/htgpio_in c 232 0
-echo -n "@@@@@@@@@@@@@   mknod /dev/input/htgpio_in c 232 0   @@@@@@@@@@@"
-exit 0
