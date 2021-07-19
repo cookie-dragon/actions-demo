433f61f2b20b39c931e199edf3c21fc0f675c9c4
Cooky.Long <lj12875@mail.haitian.com>
2021-07-19 09:15:35 +0800

    u

--- HTBOX-via-forlinx/rootfs-custom-add/etc/mdev/insert.sh
+++ HTBOX-via-forlinx/rootfs-custom-add/etc/mdev/insert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [[ -d /sys/block/*/$MDEV ]]; then
+if [ -d /sys/block/*/$MDEV ]; then
   if [[ $MDEV == mmcblk[0-9]p[0-9] ]]; then
     /opt/sbin/sys_mount_tools
     if [[ $? -eq 0 ]]; then
